b92f7b1e0d0dbfd61d0f41acc7a9c7e075411654
happykiller <fabrice.rosito@gmail.com>
2015-07-21 14:44:07 +0200

    work avatar

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
-$.Oda.Context.host = "http://localhost:3000/";
+$.Oda.Context.host = "http://localhost/ODA_HOW/";
 $.Oda.Context.rest = "http://localhost/ODA_HOW/server/";
+$.Oda.Context.resources = "http://localhost/ODA_HOW/server/resources/";
