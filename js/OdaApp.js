6f0bd63f8f1b75a0074bd1bebd0f0af7b68be834
happykiller <fabrice.rosito@gmail.com>
2016-11-30 16:51:31 +0100

    gadgetzan

--- js/OdaApp.js
+++ js/OdaApp.js
@@ -65,7 +65,7 @@ var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks"
         
         chosenClass : "",
 
-        tab_sets : ['Tous', 'Expert' , 'La Malédiction de Naxxramas', 'Gobelins et Gnomes', 'Mont Rochenoire','the-grand-tournament','league-explorers','old-gods'],
+        tab_sets : ['Tous', 'Expert' , 'La Malédiction de Naxxramas', 'Gobelins et Gnomes', 'Mont Rochenoire','the-grand-tournament','league-explorers','old-gods','gadgetzan'],
 
         /**
          * @param {Object} p_params
