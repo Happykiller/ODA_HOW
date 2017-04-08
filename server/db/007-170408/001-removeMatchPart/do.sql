a7e1eb6f02969d9161e48e96d8e7fb9f9c723134
Fabrice Rosito <fabrice.rosito@gmail.com>
2017-04-08 19:11:14 +0200

    #32

--- server/db/007-170408/001-removeMatchPart/do.sql
+++ server/db/007-170408/001-removeMatchPart/do.sql
@@ -1,12 +1,13 @@
 SET FOREIGN_KEY_CHECKS=0;
 -- --------------------------------------------------------
--- SELECT concat(`id`,';') FROM `how-api_tab_menu` WHERE 1=1 AND `lien` IN ('page_saisir_matchs.html','page_rapports_matchs.html','page_rapports_meta.html','gerer_deck.html');
+-- SELECT concat(`id`,';') FROM `how-api_tab_menu` WHERE 1=1 AND `lien` IN ('page_saisir_matchs.html','page_rapports_matchs.html','page_rapports_meta.html','page_gerer_deck.html');
+-- 20;
 -- 24;
 -- 25;
 -- 27;
 
 -- UPDATE `how-api_tab_menu_rangs_droit` SET `id_menu` = REPLACE(`id_menu`,'24;','');
 
--- DELETE FROM `how-api_tab_menu` WHERE 1=1 AND `lien` IN ('page_saisir_matchs.html','page_rapports_matchs.html','page_rapports_meta.html','gerer_deck.html');
+-- DELETE FROM `how-api_tab_menu` WHERE 1=1 AND `lien` IN ('page_saisir_matchs.html','page_rapports_matchs.html','page_rapports_meta.html','page_gerer_deck.html');
 -- --------------------------------------------------------
 SET FOREIGN_KEY_CHECKS=1;
